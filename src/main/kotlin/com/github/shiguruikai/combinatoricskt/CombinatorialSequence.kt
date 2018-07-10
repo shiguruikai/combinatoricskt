4b7673a653bc8b5fbb97c1a61583bf7c570138d4
shiguruikai <kai.shigurui@gmail.com>
2018-07-11 01:40:42 +0900

    Update KDoc

--- src/main/kotlin/com/github/shiguruikai/combinatoricskt/CombinatorialSequence.kt
+++ src/main/kotlin/com/github/shiguruikai/combinatoricskt/CombinatorialSequence.kt
@@ -9,24 +9,40 @@ import java.util.stream.StreamSupport
  * This [CombinatorialSequence] class has the [totalSize] property of the total number of sequences.
  * This sequence can be iterated only once.
  *
+ * @property totalSize Total number of elements of this sequence.
+ *
+ * @constructor Construct from sequence.
+ *
  * @see kotlin.sequences.Sequence
  * @see kotlin.sequences.constrainOnce
  */
 class CombinatorialSequence<T>(
-        /** Total number of elements in this sequence. */
         val totalSize: BigInteger,
         sequence: Sequence<T>
 ) : Sequence<T> by sequence.constrainOnce() {
 
+    /**
+     * Construct from iterator.
+     */
     constructor(size: BigInteger, iterator: Iterator<T>) : this(size, iterator.asSequence())
 
     /**
-     * @see kotlin.sequences.toList
+     * Returns a [List] containing all elements.
+     *
+     * The returned instance is [ArrayList] if [totalSize] is less than or equal to [MAX_ARRAY_SIZE],
+     * [LinkedList] otherwise.
+     *
+     * The operation is _terminal_.
      */
     fun toList(): List<T> = toMutableList()
 
     /**
-     * @see kotlin.sequences.toMutableList
+     * Returns a [MutableList] containing all elements.
+     *
+     * The returned instance is [ArrayList] if [totalSize] is less than or equal to [MAX_ARRAY_SIZE],
+     * [LinkedList] otherwise.
+     *
+     * The operation is _terminal_.
      */
     fun toMutableList(): MutableList<T> = if (totalSize <= MAX_ARRAY_SIZE) {
         toCollection(ArrayList(totalSize.intValueExact()))
@@ -35,7 +51,7 @@ class CombinatorialSequence<T>(
     }
 
     /**
-     * @see kotlin.streams.asStream
+     * @see [kotlin.streams.asStream]
      */
     fun asStream(): Stream<T> = if (totalSize <= Long.MAX_VALUE.toBigInteger()) {
         StreamSupport.stream(Spliterators.spliterator(
